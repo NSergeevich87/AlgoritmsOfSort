cf0f48564d1c1ef262c7cf1d6d1796a109ee959d
NSergeevich87 <niknikitaser@gmail.com>
2023-04-14 18:19:59 +0200

    selectsort

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,4 +1,4 @@
-﻿public static int[] SortSelection(this int[] collection)
+﻿int[] SortSelection(int[] collection)
   {
     int size = collection.Length;
     for (int i = 0; i < size - 1; i++)
@@ -14,3 +14,50 @@
     }
     return collection;
   }
+
+int[] MySortSelection(int[] array)
+{
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    int pos = i;
+
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[j] < array[pos]) pos = j;
+    }
+
+    int temp = array[i];
+    array[i] = array[pos];
+    array[pos] = temp;
+  }
+  return array;
+}
+
+int[] NewArray(int size)
+  {
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+
+    return array;
+  }
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+int[] array = NewArray(20);
+PrintArray(array);
+System.Console.WriteLine();
+//int[] selectionArray = SortSelection(array);
+//PrintArray(selectionArray);
+//System.Console.WriteLine();
+int[] mySelectionArray = MySortSelection(array);
+PrintArray(mySelectionArray);
